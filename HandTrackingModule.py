81021f19c9df606dacd409231ff8d1f7a27da30d
Vinicius Rocha Schiffler <vinicius.schiffler@t2mlab.com>
2022-03-28 21:58:15 -0300

    Adicionando barra de porcentagem e limpando código

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -40,34 +40,3 @@ class handDetector():
                     landmarkList.append([id, cx, cy])
 
         return landmarkList            
-
-    
-
-
-def main():
-    pTime = 0 # Previous time
-    cTime = 0 # Current time
-
-    #Escolhendo a webcam, poderia ser 0,1,2... dependendo d0 numero de webcams 
-    cap = cv2.VideoCapture(0)
-    detector = handDetector()
-
-    while True:
-        success, img = cap.read()
-        img = detector.findHands(img)
-        landmarksPositions = detector.findPosition(img)
-
-        cTime = time.time()
-        fps = 1/(cTime - pTime) # calculando o fps do video
-        pTime = cTime
-
-        # Colocando o fps na tela
-        # Atributos (imagem, texto, posição, font, tamanho, cor, grossura)
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3) 
-
-        cv2.imshow("Image", img)
-        cv2.waitKey(1)
-
-
-if __name__ == "__main__":
-    main()
